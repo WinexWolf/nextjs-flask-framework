03d4e8ed11f153484f1d750b0c8f3f55fd327598
winexwolf <anukriti.jaiswal01@gmail.com>
2024-04-08 02:58:15 -0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,21 @@
 "use client";
 
-import TodoForm from "@/components/TodoForm";
-import TodoItem from "@/components/TodoItem";
-import { useStore } from "@/store";
-import { useEffect } from "react";
+// pages/index.tsx
+import React from "react";
+import { useRouter, usePathname } from 'next/navigation'; // Correct import for 'app'
+import Welcome from "./components/Welcome";
+import Chat from "./chat/page";
+
 
 const Home: React.FC = () => {
-  const todos = useStore((state) => state.todos);
-  const fetchTodos = useStore((state) => state.fetchTodos);
+  const pathname = usePathname()
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
 
   return (
-    <div className="container mx-auto max-w-md p-4">
-      <TodoForm />
-      <h1 className="text-2xl font-bold mb-4">Todo List</h1>
-      {todos.length === 0 ? (
-        <p className="text-center">No Todos Found</p>
-      ) : (
-        todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
-      )}
+    <div>
+      {pathname === '/' ? <Welcome /> : <Chat />}
     </div>
   );
-};
+}
 
 export default Home;
